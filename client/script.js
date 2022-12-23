a7528918321c3b8068341c88072dd7329031a731
mbarber84 <mbarber_84@hotmail.co.uk>
2022-12-23 14:39:40 +0000

    tweaking

--- client/script.js
+++ client/script.js
@@ -1,24 +1,24 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.png';
+import bot from "./assets/bot.svg";
+import user from "./assets/user.png";
 
-const form = document.querySelector('form'); //tag name as only form
-const chatContainer = document.querySelector('#chat_container'); //id as there are multiple div's
+const form = document.querySelector("form"); //tag name as only form
+const chatContainer = document.querySelector("#chat_container"); //id as there are multiple div's
 
 let loadInterval;
 
 //loading dots for search time
 function loader(element) {
-  element.textContent = ''
+  element.textContent = "";
 
   loadInterval = setInterval(() => {
-      // Update the text content of the loading dots
-      element.textContent += '.';
+    // Update the text content of the loading dots
+    element.textContent += ".";
 
-      // If the loading has reached three dots, reset
-      if (element.textContent === '....') {
-          element.textContent = '';
-      }
-  }, 300);//number in milliseconds
+    // If the loading has reached three dots, reset
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300); //number in milliseconds
 }
 
 // Allows text to be displayed a letter at a time, like typing.
@@ -26,13 +26,13 @@ function typeText(element, text) {
   let index = 0;
 
   let interval = setInterval(() => {
-      if (index < text.length) {
-          element.innerHTML += text.charAt(index);
-          index++
-      } else {
-          clearInterval(interval);
-      }
-  }, 20);//number in milliseconds
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20); //number in milliseconds
 }
 
 // generate unique ID for each message div of bot
@@ -48,78 +48,79 @@ function generateUniqueId() {
 
 //Colour and icons to separate chat
 function chatStripe(isAi, value, uniqueId) {
-  return ( //template string = ` `
-      `
-      <div class="wrapper ${isAi && 'ai'}">
+  return (
+    //template string = ` `
+    `
+      <div class="wrapper ${isAi && "ai"}">
           <div class="chat">
               <div class="profile">
                   <img 
                     src=${isAi ? bot : user}
-                    alt="${isAi ? 'bot' : 'user'}" 
+                    alt="${isAi ? "bot" : "user"}" 
                   />
               </div>
               <div class="message" id=${uniqueId}>${value}</div>
           </div>
       </div>
   `
-  )
+  );
 }
 
-//Trigger for response 
+//Trigger for response
 const handleSubmit = async (e) => {
   e.preventDefault(); //prevents the default behavior of the browser
 
   const data = new FormData(form);
 
   // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
-  // to clear the textarea input 
+  // to clear the textarea input
   form.reset();
 
   // bot's chatstripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
-  // to focus scroll to the bottom 
+  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
-  // specific message div 
+  // specific message div
   const messageDiv = document.getElementById(uniqueId);
 
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('https://codex-im0y.onrender.com/', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-          prompt: data.get('prompt')
-      })
-  })
+  const response = await fetch("https://codex-im0y.onrender.com/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
 
-  clearInterval(loadInterval)
-  messageDiv.innerHTML = " "
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
 
   if (response.ok) {
-      const data = await response.json();
-      const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+    const data = await response.json();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
 
-      typeText(messageDiv, parsedData)
+    typeText(messageDiv, parsedData);
   } else {
-      const err = await response.text()
+    const err = await response.text();
 
-      messageDiv.innerHTML = "Something went wrong"
-      alert(err)
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
   }
-}
+};
 
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => { //enter key
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  //enter key
   if (e.keyCode === 13) {
-      handleSubmit(e)
+    handleSubmit(e);
   }
-})
-
+});
